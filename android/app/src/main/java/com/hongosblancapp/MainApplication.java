04b4db64653030cbe04b32d18d3f40568c53d55c
leareyes22 <leandroreyes_22@hotmail.com>
2022-04-20 18:43:38 -0300

    nombre del package cambiado.

--- android/app/src/main/java/com/hongosblancapp/MainApplication.java
+++ android/app/src/main/java/com/hongosblancapp/MainApplication.java
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.mobileapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.hongosblancapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
